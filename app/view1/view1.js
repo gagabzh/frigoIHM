eedba450e99617ba8e674791cd398951683cef9b
Gaga <saev.bzh@gmail.com>
2016-11-20 11:38:17 +0100

    Add link to dbb recipe

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,25 @@
 'use strict';
 
 angular.module('myApp.view1', ['ngRoute'])
-
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
   });
 }])
-
-.controller('View1Ctrl', [function() {
-
+    .factory('serviceAjax', function serviceAjax($http) {
+        return{
+            popular: function() {
+                return $http.get("http://localhost:8080/recipes");
+            }
+        };
+})
+.controller('View1Ctrl', ['$scope','serviceAjax', function($scope,serviceAjax) {
+    var loadMovies = function(){
+        serviceAjax.popular().success(function(data) {
+            $scope.recipe = data;
+            console.log($scope.recipe[0].name)
+        });
+    };
+    loadMovies();
 }]);
